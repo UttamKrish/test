fdf639f8268c10f19e887c43c0f2c5ea2a27e883
UttamKrish <85663796+UttamKrish@users.noreply.github.com>
2023-03-02 11:57:53 +0530

    python.py
    test

---
+++ pythontest.py
@@ -0,0 +1,23 @@
+import socket
+
+# create a socket object
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# bind the socket to a public host and a port
+server_socket.bind(('0.0.0.0', 5000))
+
+# become a server socket
+server_socket.listen(5)
+
+print("Server is listening on port 5000...")
+
+while True:
+    # accept connections from outside
+    (client_socket, client_address) = server_socket.accept()
+    print(f"Received connection from {client_address}")
+    
+    # do something with the client_socket
+    # ...
+    
+    # close the client socket
+    client_socket.close()
